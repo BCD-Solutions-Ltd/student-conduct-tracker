8432ee20d2d16d3ea5de911bebfaca61137eef2a
danieldcaesar <39540283+danieldcaesar@users.noreply.github.com>
2023-10-09 16:40:46 +0000

    changed models

--- App/models/student.py
+++ App/models/student.py
@@ -1,14 +1,25 @@
 from App.database import db
 
 class Student(db.Model):
-    studentid = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
+    reviews = db.relationship('Reviews', backref='student', lazy=True)
     karma = db.Column(db.Integer, default=0)
+    next_id = 1
 
-def toJson():
-    return{
-        "student ID" : self.studentid,
-        "student Name" : self.name
-    }
+    def __init__(self, student_name):
+        self.id = Student.next_id
+        self.name = student_name
+        Student.next_id += 1
+
+    def __repr__(self):
+        return f'<Student {self.id} {self.name} - {self.karma} Karma'
+
+
+    def toJson():
+        return{
+            "student ID" : self.studentid,
+            "student Name" : self.name
+        }
     
     
