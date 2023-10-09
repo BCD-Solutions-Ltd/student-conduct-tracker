e1ca5472c8ef03fbee9c7f2d64ca4af2ec233082
derwinjack <62758674+derwinjack@users.noreply.github.com>
2023-10-09 14:11:56 -0400

    Update student.py

--- App/models/student.py
+++ App/models/student.py
@@ -4,12 +4,18 @@ class Student(db.Model):
     studentid = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     karma = db.Column(db.Integer, default=0)
+    reviews = db.relationship('Review', backref='student')
 
-def toJson():
-    return{
-        "student ID" : self.studentid,
-        "student Name" : self.name
-        'karma': self.karma, 
-    }
+    def __init__(self, studentId, name):
+        self.studentId = studentId
+        self.name = name
+        self.karma = 0.0
+
+    def toJson():
+        return{
+            "student ID" : self.studentid,
+            "student Name" : self.name
+            "karma": self.karma, 
+         }
     
 
