aa65bf781ae12b6f3b3e251495cfcff826214215
derwinjack <62758674+derwinjack@users.noreply.github.com>
2023-10-09 12:10:16 -0400

    Update student.py

--- App/views/student.py
+++ App/views/student.py
@@ -6,4 +6,25 @@ from.index import index_views
 
 from App.controllers import *
 
-user_views = Blueprint('user_views', __name__, template_folder='../templates')
+student_views = Blueprint('student_views', __name__, template_folder='../templates')
+
+@student_views.route('/api/student', methods=['GET'])
+def getall_student_action():
+    users = get_all_students()
+    return jsonify(student)
+
+@student_views.route('/api/student', methods=['POST'])
+def create_student_action():
+    data = request.json
+    create_user(data['studentid'], data['name'])
+    return jsonify({'message': f"student {data['studentid']} created"})
+
+@student_views.route('/api/student', methods=['PUT'])
+def update_student_action():
+    data = request.json
+    update_student(data['studentid'], data['name'])
+    return jsonify({'message': f"student name was updated to {data['name']} "})
+
+@student_views.route('/api/student', methods=['GET'])
+def geta_student_action():
+    data = request.json
