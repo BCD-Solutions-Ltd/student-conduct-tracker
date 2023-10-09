663be99d608d88601ad0c10e9657407c79a39e73
derwinjack <62758674+derwinjack@users.noreply.github.com>
2023-10-09 14:02:36 -0400

    Update review.py

--- App/models/review.py
+++ App/models/review.py
@@ -2,14 +2,16 @@ from App.database import db
 
 class Review(db.Model):
     revid = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.studentid'), nullable=False)
     review_t = db.Column(db.Text, nullable=False)
-    votes = db.Column(db.Integer, default=0)
-    
+    upvotes = db.Column(db.Integer, default=0)
+    downvotes = db.Column(db.Integer, default=0)
 
 def toJson():
     return{
         "review ID" : self.revid,
         "student Id" : self.studentid,
-        " Review ": self.review_t
+        " Review Text ": self.review_t
+        'upvotes': self.upvotes,
+        'downvotes': self.downvotes,  
     }
