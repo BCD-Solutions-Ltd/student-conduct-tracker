9e45228d30285e68a9912fa7da23df5fdf09a45d
derwinjack <62758674+derwinjack@users.noreply.github.com>
2023-10-09 13:32:26 -0400

    Update __init__.py

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -3,12 +3,9 @@
 from .user import user_views
 from .index import index_views
 from .auth import auth_views
-from .student import *
+from .student import 
 from .review import review_views
-<<<<<<< HEAD
 
-=======
->>>>>>> 0b7ecaf8eeed9c616440bdb380709ddf970a3f41
 
 
 views = [user_views, index_views, auth_views, review_views] 
