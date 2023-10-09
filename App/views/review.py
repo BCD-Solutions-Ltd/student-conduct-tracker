325ec741f8c798d1f599c386d1d8736ad2822a18
derwinjack <62758674+derwinjack@users.noreply.github.com>
2023-10-09 12:55:48 -0400

    Update review.py

--- App/views/review.py
+++ App/views/review.py
@@ -5,8 +5,22 @@ from App.controllers import *
 
 review_views = Blueprint('review_views', __name__, template_folder='../templates')
 
-@review_views.route('/api/reviews', methods=['POST'])
+@review_views.route('/api/review', methods=['POST'])
 def create_review_action():
     data = request.json
     log_review( data['studentid'], data['review_t']):
     return jsonify({'message': f"review for: {data['studentid']} created"})
+
+
+@review_views.route('/api/review/upvote', methods=['POST'])
+def update_upvotes_action():
+    data = request.json
+    upvote_review(data['revid'])):
+    return jsonify({'message': f"review with id: {data['revid']} upvoted "})
+
+
+@review_views.route('/api/review/downvote', methods=['POST'])
+def update_downvotes_action():
+    data = request.json
+    downvote_review(data['revid'])):
+    return jsonify({'message': f"review with id: {data['revid']} downvoted "})
