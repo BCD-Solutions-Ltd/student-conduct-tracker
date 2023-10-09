bec9f0ada8422c74e5f87347912d3b04b43042b1
derwinjack <62758674+derwinjack@users.noreply.github.com>
2023-10-09 12:47:38 -0400

    Update review.py

--- App/controllers/review.py
+++ App/controllers/review.py
@@ -2,32 +2,32 @@ from App.models import Student
 from App.models import Review
 from App.database import db
 
-def log_review(student_id, content):
-    student = Student.query.get(student_id)
+def log_review(studentid, review_t):
+    student = Student.query.get(studentid)
     if student:
-        review = Review(content=content, student=student)
+        review = Review(review_t=review_t, student=student)
         db.session.add(review)
         db.session.commit()
         return review
     return None
 
 
-def upvote_review(review_id):
-    review = Review.query.get(review_id)
+def upvote_review(revid):
+    review = Review.query.get(revid)
     if review:
         review.upvotes += 1
         db.session.commit()
         return review
 
-def downvote_review(review_id):
-    review = Review.query.get(review_id)
+def downvote_review(revid):
+    review = Review.query.get(revid)
     if review:
         review.downvotes += 1
         db.session.commit()
         return review
 
-def calculate_karma(student_id):
-    student = Student.query.get(student_id)
+def calculate_karma(studentid):
+    student = Student.query.get(studentid)
     if student:
         karma = sum([review.upvotes - review.downvotes for review in student.reviews])
         student.karma = karma
