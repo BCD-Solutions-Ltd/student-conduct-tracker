1898fd773aa030bf3dee10a401ab3ede5927c9df
derwinjack <62758674+derwinjack@users.noreply.github.com>
2023-10-09 13:33:38 -0400

    Update __init__.py

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -3,10 +3,10 @@
 from .user import user_views
 from .index import index_views
 from .auth import auth_views
-from .student import 
+from .student import student_views
 from .review import review_views
 
 
 
-views = [user_views, index_views, auth_views, review_views] 
+views = [user_views, index_views, auth_views, review_views, student_views] 
 # blueprints must be added to this list
